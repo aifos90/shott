eb75d65eb77ffc1535a2e1cab0d21b04dd65157a
aifos90 <164062618+aifos90@users.noreply.github.com>
2024-03-19 20:42:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,169 @@
+#создай тут фоторедактор Easy Editor!
+import os
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog,
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+from PyQt5.QtCore import Qt # нужна константа Qt.KeepAspectRatio для изменения размеров с сохранением пропорций
+from PyQt5.QtGui import QPixmap # оптимизированная для показа на экране картинка
+
+
+from PIL import Image
+from PIL.ImageQt import ImageQt # для перевода графики из Pillow в Qt 
+from PIL import ImageFilter
+from PIL.ImageFilter import (
+   BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+   EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN,
+   GaussianBlur, UnsharpMask
+)
+app = QApplication([])
+win = QWidget()
+win.resize(700, 500)
+win.setWindowTitle('fotoshop')
+lb_image = QLabel('картинка')
+btn_dir = QPushButton('Папка')
+lw_files = QListWidget()
+
+btn_left = QPushButton('лево')
+btn_right = QPushButton('право')
+btn_flip = QPushButton('зеркало')
+btn_sharp = QPushButton('резкость')
+btn_bw = QPushButton('ч/б')
+
+row = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)
+col1.addWidget(lw_files)
+row_tools = QHBoxLayout()
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addWidget(lb_image)
+col2.addLayout(row_tools)
+row.addLayout(col1)
+row.addLayout(col2)
+win.setLayout(row)
+
+win.show()
+
+workdir = ''
+def chooseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+def filter(files, extensions):
+    result = []
+    for filename in files:
+        for ext in extensions:
+            if filename.endswith(ext):
+                result.append(filename)
+    return result
+
+def showFilenamesList():
+    try:
+        extensions = ['.png', '.jpg','.jpeg', '.bmp']
+        chooseWorkdir()
+        filenames = filter(os.listdir(workdir),extensions)
+        lw_files.clear()
+        for filename in filenames:
+            lw_files.addItem(filename)
+    except:
+        pass
+
+btn_dir.clicked.connect(showFilenamesList)
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.dir = None
+        self.filename = None
+        self.save_dir = 'Modified/'
+
+    def loadImage(self, dir, filename):
+        self.dir = dir
+        self.filename = filename
+        image_path = os.path.join(dir, filename)
+        self.image = Image.open(image_path)
+
+    def do_bw(self):
+        try:
+            self.image = self.image.convert("L")
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+        except:
+            pass
+
+    def do_sharpen(self):
+        try:
+            self.image = self.image.filter(SHARPEN)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+        except:
+            pass
+
+    def do_flip(self):
+        try:
+            self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+        except:
+            pass
+
+    def do_right(self):
+        try:
+            self.image = self.image.transpose(Image.ROTATE_270)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+        except:
+            pass
+
+    def do_left(self):
+        try:
+            self.image = self.image.transpose(Image.ROTATE_90)
+            self.saveImage()
+            image_path = os.path.join(workdir, self.save_dir, self.filename)
+            self.showImage(image_path)
+        except:
+            pass
+
+    def saveImage(self):
+        path = os.path.join(workdir, self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
+
+    def showImage(self, path):
+        lb_image.hide()
+        pixmapimage = QPixmap(path)
+        w, h = lb_image.width(), lb_image.height()
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        lb_image.setPixmap(pixmapimage)
+        lb_image.show()
+
+workimage = ImageProcessor()
+
+def showChosenImage():
+    if lw_files.currentRow() >= 0:
+        filename = lw_files.currentItem().text()
+        workimage.loadImage(workdir, filename)
+        image_path = os.path.join(workimage.dir, workimage.filename)
+        workimage.showImage(image_path)
+
+lw_files.currentRowChanged.connect(showChosenImage)
+btn_bw.clicked.connect(workimage.do_bw)
+btn_sharp.clicked.connect(workimage.do_sharpen)
+btn_flip.clicked.connect(workimage.do_flip)
+btn_right.clicked.connect(workimage.do_right)
+btn_left.clicked.connect(workimage.do_left)
+
+app.exec()
